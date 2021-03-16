64aa915f9d00b47624ad05c65d2747f3619e2930
Michelle Yung <myung@email.arizona.edu>
2021-03-16 11:38:20 -0700

    folder selection/navigation

--- django/react/src/components/Launch/FileSelect.js
+++ django/react/src/components/Launch/FileSelect.js
@@ -15,7 +15,8 @@ import {
   Button,
   Icon,
   Divider,
-  Label
+  Label,
+  Breadcrumb
 } from 'semantic-ui-react'
 
 
@@ -26,9 +27,10 @@ export default class LaunchAnalysis extends Component {
     
     this.state = {
       cyverseFiles: [],
+      currentPath: '',
     };
 
-    this.getFiles = this.getFiles.bind(this)
+    this.getFiles = this.getFiles.bind(this);
   
   };
 
@@ -37,11 +39,15 @@ export default class LaunchAnalysis extends Component {
     this.props.selectGroup(1);
   }
 
-  getFiles() {
+  getFiles(path) {
 
-    this.setState({error: null, loading:true })
+    this.setState({error: null, loading:true, currentPath: path })
   
-    axios.get('/api/files/', {})
+    axios.get('/api/files/', {
+      params: {
+        path: path
+      }
+    })
     .then(result => {
         this.setState({error: null, loading: false, cyverseFiles: result.data });
     }) 
@@ -54,100 +60,111 @@ export default class LaunchAnalysis extends Component {
 
   render() {
 
+    const { currentPath, cyverseFiles } = this.state;
+    const { selectedGroup, readType, groups } = this.props.parentState;
+
     return (
       <Grid columns={2}>
         <Grid.Column largeScreen={5}></Grid.Column>
         <Grid.Column largeScreen={11}>
           <Ref innerRef={this.contextRef}>
             <Segment.Group>
-              {/* { this.state.selectedGroup && */}
-                <Segment clearing>
-                  <h4>
-                    Select Files for Group {this.props.parentState.selectedGroup} 
-                    <Button
-                      floated='right'
-                      icon
-                      labelPosition='right'
-                      primary
-                      size='small'
-                      onClick={() => this.props.updateStep(3)}
-                    > Next
-                      <Icon name='caret right'/> 
-                    </Button>
-                  </h4>
-                </Segment>
-              {/* } */}
+              
+              <Segment clearing>
+                <h4>
+                  Select Files for Group { selectedGroup }
+                  <br></br>
+                  {/* <Breadcrumb>
+                    <Breadcrumb.Section link>Home</Breadcrumb.Section>
+                    <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    <Breadcrumb.Section link>Registration</Breadcrumb.Section>
+                    <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    <Breadcrumb.Section active>Personal Information</Breadcrumb.Section>
+                  </Breadcrumb> */}
+                  <Button
+                    floated='right'
+                    icon
+                    labelPosition='right'
+                    primary
+                    size='small'
+                    onClick={() => this.props.updateStep(3)}
+                  > 
+                    Next <Icon name='caret right'/> 
+                  </Button>
+                </h4>
+              </Segment>
+
               <Rail close position='left'>
-                  <Sticky context={this.contextRef} offset={100}>
-                    <Segment style={{overflow: 'auto', maxHeight: '70vh' }}>
-                    <Header as='h4'>
-                        {this.props.parentState.analysisType} - {this.props.parentState.readType}
-                        <Button
-                          floated='right'
-                          primary
-                          size='mini'
-                          onClick={() => this.props.updateStep(1)}
-                          >
-                          Change
-                        </Button>
-                    </Header>
-                    <Divider></Divider>
-                    <Header as='h4'>Groups</Header>
-                    { this.props.parentState.groups.length > 0 && 
-                      <>
-                      {this.props.parentState.groups.map((index, i) => {
-                        let group = this.props.parentState['group_' + index]
-                        
-                        return (
-                          <Segment key={index} 
-                                  onClick={() => this.props.selectGroup(index)}
-                                  className={classNames({
-                                  activeSegment: this.props.parentState.selectedGroup === index,
-                                })}>
-                            <h5>
-                              Group {index}
-                              <Label size='tiny'
-                                     className="floated-right">
-                                {this.props.parentState['group_' + index].length} files
-                              </Label>
-                            </h5>
-                            { this.props.parentState['group_' + index].length === 0 &&
-                              <>
-                                Select files to add.
-                              </>
-                            
-                            }
-                            { this.props.parentState['group_' + index].length > 0 &&
-                              <>
-                                {this.props.parentState['group_' + index].map((file, i) => {
-                                  let fileName = file.substring(file.lastIndexOf('/')+1);
-                                  return (
-                                    <div key={file} className="word-wrap m-b-5">
-                                      <Icon name='remove circle' color='red' onClick={() => this.props.removeFile(file, index)} />{fileName}
-                                      {i < this.props.parentState['group_' + index].length - 1 && <Divider></Divider>}
-                                    </div>
-                                  )
-                                })}
-                              </>
-                            }
-                          </Segment>
-                        )
-                      })}
-                      </>
-                    }
-                    { this.props.parentState.groups.length < 8 && 
-                      <>
-                        <Segment textAlign={"center"}
-                                className="change"
-                                key="new"
-                                onClick={() => this.props.selectGroup(this.props.parentState.groups.length + 1)}>
-                          <Button circular icon='plus'/>
+                <Sticky context={this.contextRef} offset={100}>
+                  <Segment style={{overflow: 'auto', maxHeight: '70vh' }}>
+                  <Header as='h4'>
+                      {this.props.parentState.analysisType} - {readType}
+                      <Button
+                        floated='right'
+                        primary
+                        size='mini'
+                        onClick={() => this.props.updateStep(1)}
+                        >
+                        Change
+                      </Button>
+                  </Header>
+                  <Divider></Divider>
+                  <Header as='h4'>Groups</Header>
+                  { groups.length > 0 && 
+                    <>
+                    {groups.map((index, i) => {
+                      let group = this.props.parentState['group_' + index]
+                      
+                      return (
+                        <Segment key={index} 
+                                onClick={() => this.props.selectGroup(index)}
+                                className={classNames({
+                                activeSegment: selectedGroup === index,
+                              })}>
+                          <h5>
+                            Group {index}
+                            <Label size='tiny'
+                                    className="floated-right">
+                              {this.props.parentState['group_' + index].length} files
+                            </Label>
+                          </h5>
+                          { this.props.parentState['group_' + index].length === 0 &&
+                            <>
+                              Select files to add.
+                            </>
+                          
+                          }
+                          { this.props.parentState['group_' + index].length > 0 &&
+                            <>
+                              {this.props.parentState['group_' + index].map((file, i) => {
+                                let fileName = file.substring(file.lastIndexOf('/')+1);
+                                return (
+                                  <div key={file} className="word-wrap m-b-5">
+                                    <Icon name='remove circle' color='red' onClick={() => this.props.removeFile(file, index)} />{fileName}
+                                    {i < this.props.parentState['group_' + index].length - 1 && <Divider></Divider>}
+                                  </div>
+                                )
+                              })}
+                            </>
+                          }
                         </Segment>
-                      </>
-                    }
-                    </Segment>
-                  </Sticky>
-                </Rail>
+                      )
+                    })}
+                    </>
+                  }
+                  { groups.length < 8 && 
+                    <>
+                      <Segment textAlign={"center"}
+                              className="change"
+                              key="new"
+                              onClick={() => this.props.selectGroup(groups.length + 1)}>
+                        <Button circular icon='plus'/>
+                      </Segment>
+                    </>
+                  }
+                  </Segment>
+                </Sticky>
+              </Rail>
 
               <Segment>              
                 <div className="table-container">
@@ -155,7 +172,7 @@ export default class LaunchAnalysis extends Component {
                   <Table basic='very' className="p-t-15" compact>      
                     <Table.Header>
                       <Table.Row>
-                          { this.props.parentState.selectedGroup && <Table.HeaderCell></Table.HeaderCell> }
+                          { selectedGroup && <Table.HeaderCell></Table.HeaderCell> }
                           <Table.HeaderCell>Name</Table.HeaderCell>
                           <Table.HeaderCell>Size</Table.HeaderCell>
                           <Table.HeaderCell>Last Modified</Table.HeaderCell>
@@ -163,21 +180,31 @@ export default class LaunchAnalysis extends Component {
                     </Table.Header>
 
                     <Table.Body>
-                      {this.state.cyverseFiles.map((file, i) => {
+                      { cyverseFiles.map((file, i) => {
                           return (
-                              <Table.Row obj={file} key={file.path}>
-                                { this.props.parentState.selectedGroup &&
-                                  <Table.Cell>
-                                  <Checkbox
-                                    onChange={(e, data) => this.props.handleCheck(e, data, file.path, this.props.parentState.selectedGroup)}
-                                    checked={this.props.isSelected(file.path)}/>
-                                  </Table.Cell>
+                            <Table.Row obj={file} key={file.path}>
+                              { selectedGroup &&
+                                <Table.Cell>
+                                  { file.type === 'folder' ?
+                                    <Icon name='folder'/>
+                                  :
+                                    <Checkbox
+                                      onChange={(e, data) => this.props.handleCheck(e, data, file.path, selectedGroup)}
+                                      checked={this.props.isSelected(file.path)}/>
                                   }
-                                  <Table.Cell>{file.name}</Table.Cell>
-                                  <Table.Cell>{file.size}</Table.Cell>
-                                  <Table.Cell>{file.last_updated}</Table.Cell>
-                                  
-                              </Table.Row>
+                                </Table.Cell>
+                              }
+                              <Table.Cell>
+                                { file.type === 'folder' ?
+                                    <span className="fake-link"><a onClick={() => this.getFiles(file.path)}>{file.name}</a></span>
+                                  :
+                                    <>{file.name}</>
+                                }
+                              </Table.Cell>
+                              <Table.Cell>{file.size}</Table.Cell>
+                              <Table.Cell>{file.last_updated}</Table.Cell>
+                                
+                            </Table.Row>
                           )
                       })}
                     </Table.Body>
