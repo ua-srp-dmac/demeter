de40058f19b16e0c331a3f7aab6a31ee3b7264f3
Michelle Yung <myung@email.arizona.edu>
2021-04-05 11:31:34 -0700

    add model for cyverse folder

--- django/app/admin.py
+++ django/app/admin.py
@@ -3,10 +3,26 @@ from .models import *
 
 class CyVerseAccountAdmin(admin.ModelAdmin):
     list_display = (
-        'user', 'api_token_expiration'
+        'user', 'api_token_expiration', 'folder_display'
     )
     search_fields = (
         'user', 
     )
 
+    def folder_display(self, obj):
+        return ", ".join([
+            folder.friendly_name for folder in obj.folders.all()
+        ])
+        
+    folder_display.short_description = "Folders"
+
+class CyVerseFolderAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name', 'path', 'results_path'
+    )
+    search_fields = (
+        'friendly_name', 
+    )
+
 admin.site.register(CyVerseAccount, CyVerseAccountAdmin)
+admin.site.register(CyVerseFolder, CyVerseFolderAdmin)
